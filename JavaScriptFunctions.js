50b8c64ffd755dd3233027642abf6355ecfd3a41
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-02-11 13:47:13 +0100

    Made a function for my age

--- JavaScriptFunctions.js
+++ JavaScriptFunctions.js
@@ -3,4 +3,24 @@ function updateTime() {
     document.getElementById("clock").textContent = now.toLocaleString();
 }
 
+updateTime();
 setInterval(updateTime, 1000);
+
+function updateAge() {
+    const now = new Date();
+    const birthDate = new Date(2008, 2, 13);
+
+    let age = now.getFullYear() - birthDate.getFullYear();
+
+    const monthDifference = now.getMonth() - birthDate.getMonth();
+
+    if (
+        monthDifference < 0 || (monthDifference === 0 && now.getDate() < birthDate.getDate())
+    ){
+        age--;
+    }
+
+    document.getElementById("age").textContent = age;
+}
+
+updateAge();
